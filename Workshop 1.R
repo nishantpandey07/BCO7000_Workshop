6445bc832bc4476b724ff0121e0537b66ebe6270
Nishant Pandey <nishant.pandey@vu.edu.au>
2022-02-22 21:41:02 +1100

    new file

---
+++ Workshop 1.R
@@ -0,0 +1,37 @@
+150/3
+a <- 10
+
+a
+
+b <- "Cool"
+b
+
+c <- c(3,4,7,9)
+c
+
+summary(c)
+
+summary(c(3,4,7,9))
+
+install.packages("tidyverse")
+
+library(tidyverse)
+
+rm(b)
+
+data(mtcars)
+
+head(mtcars)
+
+summary(mtcars)
+
+glimpse(mtcars)
+
+
+
+
+
+
+
+
+
